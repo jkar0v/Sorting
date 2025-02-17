9490c46b4a937b607799c0fbdd3e6f2d833680b5
MEGA <MEGA@gmail.com>
2025-02-17 17:26:50 +0200

    Add project files.

---
+++ zad 1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {   new Student("Angel", 17),
+                new Student("Yoana", 18),
+                new Student("Sofia", 16),
+                new Student("Jivko", 18),
+            };
+            BubbleSort(students);
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        static void BubbleSort(Student[] students)
+        {
+            for (int i = 0; i < students.Length - 1; i++)
+            {
+                for (int j = 0; j < students.Length - i - 1; j++)
+                {
+                    if (students[j].Age > students[j + 1].Age)
+                        (students[j], students[j + 1]) = (students[j + 1], students[j]);
+                }
+            }
+        }
+    }
+}
---
+++ zad 1/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1
+{
+    internal class Student
+    {
+        string Name {  get; set; }
+        public int Age { get; set; }
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age} years";
+        }
+    }
+}
