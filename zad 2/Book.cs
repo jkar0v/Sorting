7426dad9cdc5993ea9bf5e4ea4dea32721c852dd
Jivko Karov <jikito2007@gmail.com>
2025-02-18 21:43:51 +0200

    Zad 2 is done!

--- zad 2/Book.cs
+++ zad 2/Book.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace zad_2
 {
     internal class Book
     {
-        string Title { get; set; }
+        public string Title { get; set; }
         string Author { get; set; }
         public Book(string title, string author)
         {
             Title = title;
             Author = author;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Author}";
         }
     }
 }
--- zad 2/Program.cs
+++ zad 2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book[] books = new Book[]
+            {
+                new Book("Silent Harbor", "F. Scott Fitzgerald"),
+                new Book("Whispers of Maycomb", "Harper Lee"),
+                new Book("Empire's End", "George Orwell"),
+                new Book("A Dance of Hearts", "Jane Austen"),
+                new Book("Beyond the Pines", "J.D. Salinger"),
+                new Book("Legends of Eldenwood", "J.R.R. Tolkien"),
+                new Book("The Dragon's Path", "J.R.R. Tolkien")
+            };
+            SelectionSort(books);
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        static void SelectionSort(Book[] books)
+        {
+            int n = books.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (books[j].Title[0] < books[minIndex].Title[0])
+                    {
+                        minIndex = j;
+                    }
+                    if(books[j].Title[0] == books[minIndex].Title[0])
+                    {
+                        if (books[j].Title[1] < books[minIndex].Title[0])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                }
+                (books[i], books[minIndex]) = (books[minIndex], books[i]);
+            }
         }
     }
 }
