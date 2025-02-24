16d26bf49d60cf3c4caa98868032d777f7fc7f8b
Jivko Karov <jikito2007@gmail.com>
2025-02-24 10:09:22 +0200

    Added zad 4

---
+++ zad 4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[]
+            {
+                new Student ("Ана", 5.50),
+                new Student ("Борис", 4.80),
+                new Student ("Васил", 6.00),
+                new Student ("Ивана", 5.50),
+                new Student ("Галя", 5.00)
+            };
+            InsertionSort(students);
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.Name} - {student.AverageMark:f2}");
+            }
+        }
+        static void InsertionSort(Student[] array)
+        {
+            int n = array.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                Student key = array[i];
+                int j = i - 1;
+                while (j >= 0 &&
+                      (array[j].AverageMark > key.AverageMark ||
+                      (array[j].AverageMark == key.AverageMark && array[j].Name.CompareTo(key.Name) > 0)))
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ zad 4/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_4
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public double AverageMark { get; set; }
+        public Student(string name, double averageMark)
+        {
+            Name = name;
+            AverageMark = averageMark;
+        }
+    }
+}
